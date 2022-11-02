2b3d0865a20dc011dd44fa88a673d540ddd31c0b
Huascar <huascar.sanchez@gmail.com>
2022-11-02 16:39:53 -0700

    minor changes to pandas

--- funcs/pandas.py
+++ funcs/pandas.py
@@ -12,20 +12,9 @@ from .console import progress_display
 
 def _check_input_dataframe(input_df: pd.DataFrame) -> None:
   # Empty Dataframe check
-  assert input_df.shape[0]>0 and input_df.shape[1]>0 , "DataFrame is Empty"
+  assert input_df.shape[0] > 0 and input_df.shape[1] > 0 , "DataFrame is Empty"
   # duplicate columns check
   assert len(input_df.columns.values)==len(set(input_df.columns.values)) , "DataFrame has duplicate columns"
-
-
-def __particular_values_per_column(input_df: pd.DataFrame, values: list) -> pd.DataFrame:
-  _check_input_dataframe(input_df)
-  counts = {}
-  for idx in input_df.columns.values:
-    cnt = np.sum(input_df[idx].isin(values).values)
-    counts[idx] = cnt
-  ctr = pd.DataFrame([counts]).T
-  ctr_2 = ctr.rename(columns={0: '# Values as %s'%values})
-  return ctr_2
 
 
 def get_column_names(input_df: pd.DataFrame, sorted: bool = True) -> ty.List[ty.Any]:
@@ -68,57 +57,10 @@ def get_column_datatypes(input_df: pd.DataFrame) -> pd.DataFrame:
   return ctr
 
 
-def get_most_common_value(input_df: pd.DataFrame) -> pd.DataFrame:
-  # for simple cases use df.mode().T
-  _check_input_dataframe(input_df)
-  total_rows = input_df.shape[0]
-  columns, modes, counts = list(), list(), list()
-  for column in input_df.columns:
-    count = input_df[column].value_counts().max()
-    mode = input_df[column].value_counts().index[0]
-    columns.append(column)
-    modes.append(mode)
-    counts.append(count)
-  description = pd.DataFrame(
-    index=columns,
-    data={"MostFrequentValue": modes, 
-          "MostFrequentValueCount": counts})
-  description["MostFrequentValue %"] = 100 * description["MostFrequentValueCount"] / total_rows
-  description.index.names = ["Column"]
-  return description
-
-
-def describe_dataframe(input_df: pd.DataFrame) -> pd.DataFrame:
-  _check_input_dataframe(input_df)
-
-  numeric_only_df = input_df.select_dtypes(np.number)
-  mis_val = numeric_only_df.isnull().sum()
-  mis_val_percent = 100 * mis_val / len(numeric_only_df)
-  particular_ctr = __particular_values_per_column(numeric_only_df, [0])
-  unique_ctr = count_distinct_values(numeric_only_df)
-  statistical_summary = numeric_only_df.describe().T
-  datatypes = get_column_datatypes(numeric_only_df)
-  most_common_values = get_most_common_value(numeric_only_df)
-  skewed = pd.DataFrame(numeric_only_df.skew()).rename(columns={0: 'skew'})
-  mis_val_table = pd.concat([
-    mis_val,
-    mis_val_percent,
-    unique_ctr,
-    particular_ctr,
-    datatypes,
-    skewed,
-    statistical_summary,
-    most_common_values], axis=1)
-  mis_val_table_ren_columns = mis_val_table.rename(
-    columns = {0 : 'Missing Values', 1 : '% missing of Total Values'})
-
-  return mis_val_table_ren_columns
-
-
 def find_correlated_pairs(input_df: pd.DataFrame, threshold: float = 0.8) -> pd.DataFrame:
   _check_input_dataframe(input_df)
   corr = input_df.corr()
-  corr = corr.where(np.triu(np.ones(corr.shape), k=1).astype(np.bool))
+  corr = corr.where(np.triu(np.ones(corr.shape), k=1).astype(bool))
   corr = corr.stack().reset_index()
   corr.columns = ['var1', 'var2', 'corr']
   corr = corr.loc[corr['corr'].abs() > threshold]
