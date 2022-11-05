4e415d30d5dfaae75284278d41e1398f8d626ea0
Huascar <huascar.sanchez@gmail.com>
2022-11-04 18:05:11 -0700

    adds alignment API to arrays

--- arrays.py
+++ arrays.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import typing as ty
+from collections import namedtuple
 
+from .console import new_progress_display, stderr
 from .pinstall import install as install_package
 
 try:
@@ -12,9 +14,11 @@ except ImportError:
 
 try:
   from scipy.spatial import procrustes
+  from scipy.spatial.distance import cdist, pdist, squareform
 except ImportError:
   install_package('scipy')
   from scipy.spatial import procrustes
+  from scipy.spatial.distance import cdist, pdist, squareform
 
 try:
   from sklearn.decomposition import PCA
@@ -163,13 +167,231 @@ def summarize_vectors_sequence(vecs_seq: np.ndarray, alpha: float = 0.5) -> np.n
 
 
 def summarize_vectors_sequence_by_weighted_mixing(vecs_seq: np.ndarray, alpha: float = 0.5) -> np.ndarray:
+  """Summarize a sequence of vectors by taking the weighted similarity of their vectors.
+  Args:
+    vecs: a sequence of vectors
+    alpha: the similarity mixing factor
+  Returns:
+    a vector
+  """
   U = np.array(vecs_seq[0])
-
-  S = [cosine_similarity(U, vec_j) * np.array(vec_j) for vec_j in vecs_seq[1:]]
-  S = np.add.reduce(S)
+  S = np.add.reduce([cosine_similarity(U, vec_j) * np.array(vec_j) for vec_j in vecs_seq[1:]])
   N = np.sum([cosine_similarity(U, vec_j) for vec_j in vecs_seq[1:]])
   
   return U * alpha + (1 - alpha) * (S / N)
+
+
+def apply_seq_kernel(vec: ty.Union[ty.List[float], np.ndarray], kernel_radius: int = 1, epsilon: float = 0.01) -> np.ndarray:
+  # apply Gaussian kernel to a vector
+  if isinstance(vec, list):
+    vec = np.array(vec)
+
+  kernels = squareform(pdist(np.expand_dims(np.arange(vec.size), axis=-1)))
+  
+  # Calculate graph kernels with cutoff of epsilon at the kernel_radius.
+  kernels = np.exp((kernels * np.log(epsilon)) / kernel_radius)
+  kernels[kernels < epsilon] = 0
+
+  # Normalize kernels by dividing by row sums.
+  kernels = kernels / np.expand_dims(np.sum(kernels, axis=-1), axis=-1)
+  
+  # Updates sequence embeddings using kernel
+  c_emb_prime = np.dot(kernels, vec)
+  
+  return c_emb_prime
+
+
+def build_distance_matrix(vec_i: np.ndarray, vec_j: np.ndarray) -> np.ndarray:
+  return 1.0 - cdist(vec_i, vec_j, metric='cosine')
+
+
+def build_scoring_matrix(a: np.ndarray, w_i: float = 1.0, w_j: float = 1.0, epsilon: float = 0.01) -> np.ndarray:
+  # Pad distance matrix
+  sa = np.pad(a, ((1,0),(1,0)), 'constant', constant_values = 0)
+  
+  # Calculate gap weight kernels
+  dims = a.shape
+  w_i_ = [w_i * np.exp((i * np.log(epsilon)) / dims[0]) for i in reversed(range(dims[0] + 1))]
+  w_j_ = [w_j * np.exp((j * np.log(epsilon)) / dims[1]) for j in reversed(range(dims[1] + 1))]
+  
+  # Updates scoring matrix according to policy
+  for i in range(1, dims[0] + 1):
+    for j in range(1, dims[1] + 1):
+      inputs = [
+        # Top Left + Bottom Right
+        (sa[i, j] + sa[i - 1,j - 1]),
+        # Max of all previous values in column - column gap weight
+        np.max(sa[:i,j]) - w_i_[i - np.argmax(sa[:i, j])],
+        # Max of all previous values in row - row gap weight
+        np.max(sa[i,:j]) - w_j_[j - np.argmax(sa[i, :j])],
+        # Zero
+        0]
+      sa[i, j] = np.max(inputs)
+  return sa
+
+
+def perform_traceback(sa: np.ndarray, k: ty.Optional[int] = 100) -> np.ndarray:
+  # Sort scoring matrix values in descending order; Save coordinates in look up table.
+  sorted_args = np.argsort(sa.flatten())[::-1]
+  coords = [(i, j) for i in range(sa.shape[0]) for j in range(sa.shape[1])]
+  
+  # Perform traceback until all coords have been visited
+  tracebacks = []
+  seen = []
+  route = []
+  
+  for idx in sorted_args:
+    # matrix indices
+    i, j = coords[idx]
+    
+    flag = True
+    score = sa[i, j]
+
+    while(flag):
+      # Route connects to other traceback
+      if (i, j) in seen:
+        tracebacks.append([route, (i, j)])
+        route = []
+        break
+      
+      route.append((i, j))
+      seen.append((i, j))
+      
+      # Route terminates at zero
+      if sa[i, j] == 0:
+        tracebacks.append([route, []])
+        route = []
+
+      # Select path direction
+      kernel = [sa[i - 1, j], sa[i,j - 1], sa[i - 1, j - 1], sa[i, j]]
+      m = np.argmax(kernel)
+      
+      # Move to next gap
+      if m == 0:
+        # Terminate route if score is less than gap value
+        if score > sa[i - 1, j]:
+          i -= 1
+          score += sa[i, j]
+        else:
+          tracebacks.append([route, []])
+          route = []
+          break
+      elif m == 1:
+        # Terminate route if score is less than gap value
+        if score > sa[i, j - 1]:
+          j -= 1
+          score += sa[i, j]
+        else:
+          tracebacks.append([route, []])
+          route = []
+          break
+      # Move to next hit
+      elif m in [2, 3]:
+        i -= 1
+        j -= 1
+        score += sa[i, j]
+      
+      # Stop at zero or if route is too long
+      if i < 0 or j < 0:
+        break
+
+
+  # Return alignments with length greater than 1 in order as they are found.
+  if k is None:
+    k = len(tracebacks)
+
+  alignments = [] # a collection of index tuples
+  for _ in tracebacks:
+    # check length of routes
+    if len(_[0]) > 1:
+      r = [(i - 1, j - 1) for (i, j) in _[0]]
+      alignments.append(r[:-1])
+    if len(alignments) == k:
+      break
+
+  return alignments
+
+
+def score_alignment(alignment: np.ndarray, seq_x: np.ndarray, seq_y: np.ndarray, k: int) -> float:
+  # Find gaps and hits, and gather feature vectors
+  temp_i = []
+  temp_j = []
+  
+  i = -1
+  j = -1
+  segment_a = []
+  segment_b = []
+
+  for each_idx_pair in alignment:
+    if each_idx_pair[0] != i:
+      temp_i.append(1)
+      i = each_idx_pair[0]
+    else: temp_i.append(0.0)
+    if each_idx_pair[1] != j:
+      temp_j.append(1)
+      j = each_idx_pair[1]
+    else: temp_j.append(0.0)
+    segment_a.append(seq_x[each_idx_pair[0]])
+    segment_b.append(seq_y[each_idx_pair[1]])
+  
+  # Calculate similarity score
+  mask = np.array(temp_i) * np.array(temp_j)
+  similarity = 2 - cdist(segment_a, segment_b, 'cosine').diagonal()
+  score = (similarity * mask) / (2 * len(alignment)) * (np.sum(mask) / len(seq_y)) * k * len(seq_y)
+  
+  if not score or np.isnan(score):
+    stderr.print('alignment score is NaN', style='yellow')
+    score = 0.0
+
+  return score[0]
+
+
+def process_alignments(
+  top_alignments: np.ndarray, 
+  top_scores: np.ndarray, 
+  input_vecs: ty.List[np.ndarray],
+  vec_index: dict) -> list:
+  
+  assert len(input_vecs) == 2, 'Only two sequences can be aligned at a time.'
+  v_i = input_vecs[0]
+  v_j = input_vecs[1]
+  Alignment = namedtuple('Alignment', ['score', 'X', 'Y'])
+  
+  results = []
+  with new_progress_display(console=stderr) as progress:
+    task = progress.add_task('Process alignments ...', total=len(top_alignments))
+    for i, a in enumerate(top_alignments):
+      X = []
+      Y = []
+      l = -1
+      r = -1
+      for each_idx_pair in reversed(a):
+        # process the left index
+        if each_idx_pair[0] != l:
+          if vec_index:
+            cands_l = vec_index.get(tuple(v_i[each_idx_pair[0]]), None)
+            cands_l = [tpl[1] for tpl in cands_l if tpl[0] == 0]
+            X += [cands_l[0] if len(cands_l) > 0 else None]
+          else:
+            X.append(v_i[each_idx_pair[0]])
+          l = each_idx_pair[0]
+        else:
+          X.append('GAP')
+
+        # process the right index
+        if each_idx_pair[1] != r:
+          if vec_index:
+            cands_j = vec_index.get(tuple(v_j[each_idx_pair[1]]), None)
+            cands_j = [tpl[1] for tpl in cands_j if tpl[0] == 1]
+            Y += [cands_j[0] if len(cands_j) > 0 else None]
+          else:
+            Y.append(v_j[each_idx_pair[1]])
+          r = each_idx_pair[1]
+        else:
+          Y.append('GAP')
+      results += [Alignment(top_scores[i], np.array(X), np.array(Y))]
+      progress.update(task, advance=1)
+  return results
 
 
 if __name__ == "__main__":
