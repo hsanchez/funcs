690070b913eaf0166d8c5b42e6133ede127dd326
Huascar <huascar.sanchez@gmail.com>
2022-11-04 22:36:03 -0700

    adds init file

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .arrays import *
+from .colabs import *
+from .console import *
+from .data import *
+from .gdrive import *
+from .nlp import *
+from .pinstall import *
+from .plotz import *
+
+__version__ = "0.0.1"
