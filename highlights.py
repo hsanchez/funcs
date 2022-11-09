c9c7e09d237c25f5c4f0daef392f460e50a34da7
Huascar <huascar.sanchez@gmail.com>
2022-11-08 20:32:39 -0800

    add highlight fn

--- highlights.py
+++ highlights.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+import pandas as pd
+import typing as ty
+
 
 # style cells in pandas tables
 def style_negative(v, props=''):
@@ -18,6 +21,13 @@ def green_values(x):
 
 def pink_values(x):
   return np.where(((x >= 0.1) & (x < 0.3)), f"background-color: #ffe6e6;", None)
+
+
+def highlight(input_df: pd.DataFrame, highlighter: ty.Callable, color: str = None) -> None:
+  if color is None:
+    input_df.style.apply(highlighter)
+  else:
+    input_df.style.apply(highlighter, color=color)
 
 
 if __name__ == "__main__":
