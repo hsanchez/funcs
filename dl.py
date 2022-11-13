9fa9f0e3271af3c2be8cd63176f25c3f5a6d6dba
Huascar <huascar.sanchez@gmail.com>
2022-11-12 20:18:15 -0800

    Adds Skipgram model and its accelerator

--- dl.py
+++ dl.py
@@ -3,8 +3,10 @@
 import typing as ty
 
 import numpy as np
+from tqdm import tqdm
 
 from .arrays import ArrayLike, multidimensional_shifting
+from .common import take
 from .console import new_progress_display, quiet_stderr, stderr
 from .modules import install as install_package
 
@@ -32,9 +34,93 @@ except ImportError:
   from accelerate import utils as accelerate_utils
 
 
+# defining the Dataset class
+class SkipgramData(Dataset):
+  def __init__(self, skipgram_data):
+    self.data = skipgram_data
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, index):
+    return self.data[index]
+
+
+class SkipgramModel(nn.Module):
+  def __init__(self, embedding_size: int, act2idx: dict, bias: bool = False, device = None) -> None:
+    super(SkipgramModel, self).__init__()
+    acts_size = len(act2idx)
+    self.embedding = nn.Embedding(acts_size, embedding_size)
+    self.W = nn.Linear(embedding_size, embedding_size, bias=bias) 
+    self.WT = nn.Linear(embedding_size, acts_size, bias=bias)
+    self.vocab = act2idx
+    self.device = device
+  
+  def forward(self, X):
+    embeddings = self.embedding(X)
+    hidden_layer = nn.functional.relu(self.W(embeddings)) 
+    output_layer = self.WT(hidden_layer)
+    return output_layer
+  
+  def get_embedding(self, activity: str) -> torch.Tensor:
+    if activity not in self.vocab:
+      raise ValueError(f"Activity {activity} not in index")
+    if self.device is not None:
+      act_vec = torch.tensor([self.act2idx[activity]]).to(self.device)
+    else:
+      act_vec = torch.tensor([self.act2idx[activity]])
+    return self.embedding(act_vec).view(1,-1)
+
+
 def set_random_seed(seed):
   """Set random seed for reproducibility."""
   accelerate_utils.set_seed(seed)
+
+
+def get_dataloaders_per_time_slice(
+  train_test_by_time_slice: ty.List[tuple], 
+  time_slices: ArrayLike, 
+  batch_size: int = 10) -> ty.List[tuple]:
+  if isinstance(time_slices, np.ndarray):
+    time_slices = time_slices.tolist()
+
+  train_test_dataloaders_by_time_slice = []
+  for week in time_slices:
+    train_w, test_w = train_test_by_time_slice[time_slices.index(week)]
+
+    train_dataset = SkipgramData(train_w)
+    test_dataset  = SkipgramData(test_w)
+
+    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_data_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+    train_test_dataloaders_by_time_slice.append((train_data_loader, test_data_loader))
+  return train_test_dataloaders_by_time_slice
+
+
+def accelerate_model(
+  train_test_dataloaders: ty.List[tuple],
+  timeline_slices: ArrayLike, 
+  act2idx: dict, 
+  embedding_size: int = 120, 
+  learn_rate: float = 0.001, 
+  accelerator: Accelerator = Accelerator()) -> ty.Tuple[Accelerator, ty.List[tuple]]:
+  
+  if isinstance(timeline_slices, np.ndarray):
+    timeline_slices = timeline_slices.tolist()
+  
+  accelerations = []
+  for t in timeline_slices:
+    train_data_loader, test_data_loader = train_test_dataloaders[timeline_slices.index(t)]
+    model = SkipgramModel(embedding_size, act2idx, accelerator.device)
+    model, optimizer, train_data, test_data = accelerator.prepare(
+      model,
+      optim.Adam(model.parameters(), lr=learn_rate),
+      train_data_loader,
+      test_data_loader)
+    accelerations += [(model, optimizer, train_data, test_data)]
+  
+  return accelerations, accelerator
 
 
 def send_to_tensor(ctx, ctx2idx: dict, device=torch.device) -> torch.Tensor:
@@ -74,6 +160,9 @@ def get_train_test_data_per_period(
   if progress_bar:
     the_console = stderr
   
+  if isinstance(time_slices, np.ndarray):
+    time_slices = time_slices.tolist()
+  
   train_test_data = []
   with new_progress_display(the_console) as progress:
     task = progress.add_task("Collecting data in time slices ...", total=len(sliced_skipgrams))
@@ -84,7 +173,6 @@ def get_train_test_data_per_period(
       progress.update(task, advance=1)
   
   return np.array(train_test_data)
-
 
 
 def get_annotated_coordinates_from_model(activities: ArrayLike, trained_model: ty.Any, act2abbr: dict) -> tuple:
@@ -98,6 +186,10 @@ def get_annotated_coordinates_from_model(activities: ArrayLike, trained_model: t
       x = embedding.detach().data.numpy()
       y = embedding.detach().data.numpy()
     yield act2abbr[activity], x, y
+
+
+def take_n_from_data_loader(n: int, t: int, train_test_dataloaders_by_time_slice: ty.List[tuple]) -> list:
+  return take(n, train_test_dataloaders_by_time_slice[t][0])
 
 
 if __name__ == "__main__":
