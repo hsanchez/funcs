649e4669b7c105ed79b47adfd6cab94776017d3d
Huascar <huascar.sanchez@gmail.com>
2022-11-13 14:07:24 -0800

    fixes union bug

--- data.py
+++ data.py
@@ -688,7 +688,7 @@ def build_diachronic_dataframe(
     contributor_to_activity=contributor_group_idx,
     maintainer_to_activity=maintainer_group_idx)
   
-  rel_activities = np.union1d(maintainer_activities + contributor_activities)
+  rel_activities = np.union1d(maintainer_activities, contributor_activities)
   
   @with_status(console=the_console, prefix='Build diachronic dataframe')
   def generate_diachronic_df(df: pd.DataFrame, r_activities: ArrayLike, a_name_col: str) -> tuple:
