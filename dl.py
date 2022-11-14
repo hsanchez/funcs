db2c825991d8609e3442f086dde0a9eccac85525
Huascar <huascar.sanchez@gmail.com>
2022-11-14 10:42:59 -0800

    fixes wrong param issue

--- dl.py
+++ dl.py
@@ -120,7 +120,7 @@ class AlignedW2V:
     
     for time_slice in timeline_slices:
       time_slice_model = timeline_slice_models[timeline_slices.index(time_slice)]
-      activity_embedding = np.array([get_embedding_safely(time_slice_model.embedding, act, device).detach().cpu().numpy()[0] for act in self.act2idx])
+      activity_embedding = np.array([get_embedding_safely(time_slice_model.embedding, self.act2idx[act], device).detach().cpu().numpy()[0] for act in self.act2idx])
       # if torch.cuda.is_available():
       #   activity_embedding = np.array([time_slice_model.get_embedding(act).detach().cpu().numpy()[0] for act in self.act2idx])
       # else:
